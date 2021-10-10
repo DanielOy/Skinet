e8dad104167149cf33f2e51cc7f7f56263d3ada4
Daniel Oy <daniel-oy-mis@hotmail.com>
2021-10-09 22:16:28 -0500

    Core and Infrastructure projects added

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,5 @@
-﻿using API.Data;
-using API.Entities;
+﻿using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
