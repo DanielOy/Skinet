4c638bee275f9fe4088e16117984577fb1d9ba27
Daniel Oy <daniel-oy-mis@hotmail.com>
2021-10-31 20:59:01 -0600

    Add basic changes in the client application Nav bar added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      this.products = response.data;
+      console.log(response);
+    }, error => {
+        console.error(error);
+      });
+  }
+
 }
